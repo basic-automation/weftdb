4059a820514a438432656a8ba5096d6b2cc88228
Justin Icenhour <creative515.justin@gmail.com>
2023-04-27 10:57:30 -0500

    update deps

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use bigdecimal::BigDecimal;
 use dsm_pattern::*;
 use num_bigint::BigUint;
 use router::*;
+use serde_json::json;
 use serde_json::Value;
 use std::collections::HashMap;
-use std::{env, str::FromStr, thread};
+use std::{str::FromStr, thread};
 use tokio::time::{sleep, Duration};
 use uuid::Uuid;
-use serde_json::json;
 //use futures::future::join_all;
+use dsm_config::Configuration;
 use reqwest::StatusCode;
 
 mod router;
@@ -17,10 +18,14 @@ mod router;
 #[tokio::main]
 async fn main() {
 	let server = thread::spawn(async move || {
+		let config = match Configuration::open("../dsm.toml").await {
+			Ok(config) => config,
+			Err(e) => panic!("Error: Failed to open configuration file: {}", e),
+		};
 		println!("server running...");
 		let app = router();
 
-		let port = env::var("PORT").unwrap_or("8518".to_string());
+		let port = config.dsm.ports.patterner;
 		let address = format!("0.0.0.0:{}", port);
 		println!("Listening on address {}", address);
 
@@ -33,76 +38,76 @@ async fn main() {
 
 		loop {
 			// create new pattern dictionary
-                        println!("Getting patterns...");
-                        let patterns = get_patterns().await;
-                        println!("Number of patterns: {}", patterns.len());
+			println!("Getting patterns...");
+			let patterns = get_patterns().await;
+			println!("Number of patterns: {}", patterns.len());
 
-                        let dicts: Vec<String> = patterns.keys().cloned().collect();
-                        let length = dicts.len();
-                        println!("Number of dictionaries: {}", length);
-                        
-                        //let mut threads = Vec::new();
-                        let mut i = 0_usize;
-                        loop {
-                                if i == length {
-                                        break;
-                                }
-                                let name = dicts[i].clone();
-                                let mut dictionary = get_dictionary(&name).await.unwrap();
+			let dicts: Vec<String> = patterns.keys().cloned().collect();
+			let length = dicts.len();
+			println!("Number of dictionaries: {}", length);
 
-                                //let thread = std::thread::spawn(async move || {
-                                        println!("Processing Dictionary: {}", dictionary.name.clone());
+			//let mut threads = Vec::new();
+			let mut i = 0_usize;
+			loop {
+				if i == length {
+					break;
+				}
+				let name = dicts[i].clone();
+				let mut dictionary = get_dictionary(&name).await.unwrap();
 
-                                        // get batches
-                                        let patterns = get_patterns().await;
-                                        let patterns = patterns.get(&name).unwrap().clone();
+				//let thread = std::thread::spawn(async move || {
+				println!("Processing Dictionary: {}", dictionary.name.clone());
 
-                                        // add patterns to dictionary
+				// get batches
+				let patterns = get_patterns().await;
+				let patterns = patterns.get(&name).unwrap().clone();
 
-                                        // start timer
-                                        let start = std::time::Instant::now();
-                                        println!("Adding {} patterns to dictionary...", patterns.len());
-                                        dictionary.add_patterns(patterns.clone()).await;
+				// add patterns to dictionary
 
-                                        //stop timer
-                                        let duration = start.elapsed();
-                                        println!("Adding patterns took: {:?}", duration);
+				// start timer
+				let start = std::time::Instant::now();
+				println!("Adding {} patterns to dictionary...", patterns.len());
+				dictionary.add_patterns(patterns.clone()).await;
 
-                                        println!("Dictionary size: {}", dictionary.patterns.clone().unwrap().len());
+				//stop timer
+				let duration = start.elapsed();
+				println!("Adding patterns took: {:?}", duration);
 
-                                        // enforce variability
-                                        // start timer
-                                        let start = std::time::Instant::now();
-                                        let patts = dictionary.patterns.clone().unwrap();
-                                        println!("Enforcing dictionary variability: {} patterns in dictionary...", patts.len());
-                                        dictionary.enforce_variability().await;
+				println!("Dictionary size: {}", dictionary.patterns.clone().unwrap().len());
 
-                                        // stop timer
-                                        let duration = start.elapsed();
-                                        println!("Enforcing variability took: {:?}", duration);
+				// enforce variability
+				// start timer
+				let start = std::time::Instant::now();
+				let patts = dictionary.patterns.clone().unwrap();
+				println!("Enforcing dictionary variability: {} patterns in dictionary...", patts.len());
+				dictionary.enforce_variability().await;
 
-                                        // save logs
-                                        dictionary.finish().await;
+				// stop timer
+				let duration = start.elapsed();
+				println!("Enforcing variability took: {:?}", duration);
 
-                                        println!("Dictionary size: {}", dictionary.patterns.clone().unwrap().len());
+				// save logs
+				dictionary.finish().await;
 
-                                        //let dictionary_json = serde_json::to_string(&dictionary).unwrap();
+				println!("Dictionary size: {}", dictionary.patterns.clone().unwrap().len());
 
-                                        delete_batches(patterns).await;
+				//let dictionary_json = serde_json::to_string(&dictionary).unwrap();
 
-                                        save_dictionary(&dictionary).await.unwrap();
-                                //});
+				delete_batches(patterns).await;
 
-                                //threads.push(thread);
-                                i += 1;
-                        }
+				save_dictionary(&dictionary).await.unwrap();
+				//});
 
-                        /* let mut joined = Vec::new();
-                        for thread in threads {
-                                joined.push(thread.join().unwrap());
-                        }
+				//threads.push(thread);
+				i += 1;
+			}
 
-                        join_all(joined).await; */
+			/* let mut joined = Vec::new();
+			for thread in threads {
+					joined.push(thread.join().unwrap());
+			}
+
+			join_all(joined).await; */
 			sleep(Duration::from_secs(1)).await;
 		}
 	});
@@ -112,8 +117,14 @@ async fn main() {
 }
 
 async fn get_patterns() -> HashMap<String, Vec<Pattern>> {
+        let config = match Configuration::open("../dsm.toml").await {
+		Ok(config) => config,
+		Err(e) => panic!("Error: Failed to open configuration file: {}", e),
+	};
 	let client = reqwest::Client::new();
-	let url = "http://127.0.0.1:8515/bucket/batch?count=1000&page=1";
+        let throttle: u64 = config.dsm.throttles.patterner.unwrap_or(10000);
+        let database_port = config.dsm.database.port;
+	let url = format!("http://127.0.0.1:{database_port}/bucket/batch?count={throttle}&page=1");
 
 	let response = client.get(url).send().await;
 	match response {
@@ -131,7 +142,7 @@ async fn get_patterns() -> HashMap<String, Vec<Pattern>> {
 				.map(|batch| {
 					let k: String = serde_json::from_value(batch["key"].clone()).unwrap();
 					let o = Occurrence {
-						measurement_bucket: serde_json::from_value(batch["value"]["measurement_bucket"].clone()).unwrap(),
+						measurement_bucket: serde_json::from_value(batch["value"]["protocol_address"].clone()).unwrap(),
 						source: serde_json::from_value(batch["value"]["source"].clone()).unwrap(),
 						start: serde_json::from_value(batch["value"]["start"].clone()).unwrap(),
 						end: serde_json::from_value(batch["value"]["end"].clone()).unwrap(),
@@ -149,20 +160,20 @@ async fn get_patterns() -> HashMap<String, Vec<Pattern>> {
 				})
 				.collect();
 
-                        // sort patterns by source
-                        let mut sorted_patterns: HashMap<String, Vec<Pattern>> = HashMap::new();
-                        for pattern in patterns {
-                                let source = pattern.occurrences.get(&BigUint::from(0_u8)).unwrap().measurement_bucket.clone();
-                                if sorted_patterns.contains_key(&source) {
-                                        let mut patts = sorted_patterns.get(&source).unwrap().clone();
-                                        patts.push(pattern);
-                                        sorted_patterns.insert(source, patts);
-                                } else {
-                                        sorted_patterns.insert(source, vec![pattern]);
-                                }
-                        }
+			// sort patterns by source
+			let mut sorted_patterns: HashMap<String, Vec<Pattern>> = HashMap::new();
+			for pattern in patterns {
+				let source = pattern.occurrences.get(&BigUint::from(0_u8)).unwrap().measurement_bucket.clone();
+				if sorted_patterns.contains_key(&source) {
+					let mut patts = sorted_patterns.get(&source).unwrap().clone();
+					patts.push(pattern);
+					sorted_patterns.insert(source, patts);
+				} else {
+					sorted_patterns.insert(source, vec![pattern]);
+				}
+			}
 
-                        sorted_patterns
+			sorted_patterns
 		}
 		Err(e) => {
 			println!("Error: {}", e);
@@ -172,70 +183,68 @@ async fn get_patterns() -> HashMap<String, Vec<Pattern>> {
 }
 
 async fn get_dictionary(name: &str) -> Result<Dictionary, String> {
-        let client = reqwest::Client::new();
-        let url = format!("http://127.0.0.1:8515/bucket/dictionary/{}", name);
-        println!("Getting dictionary: {}", url.clone());
-        let response = client.get(url).send().await;
-        let response = match response {
-                Ok(response) => response,
-                Err(e) => return Err(format!("Error: No response from database: {}", e)),
-        };
+	let client = reqwest::Client::new();
+	let url = format!("http://127.0.0.1:8515/bucket/dictionary/{}", name);
+	println!("Getting dictionary: {}", url.clone());
+	let response = client.get(url).send().await;
+	let response = match response {
+		Ok(response) => response,
+		Err(e) => return Err(format!("Error: No response from database: {}", e)),
+	};
 
-
-        match response.status() {
-                StatusCode::NOT_FOUND => {
-                        println!("Creating new dictionary...");
-                        Ok(Dictionary::new(name, Constraints::default(), false))
-                },
-                StatusCode::OK => {
-                        let body: Value = response.json().await.unwrap();
-                        let dictionary: Dictionary = match serde_json::from_value::<Dictionary>(body[name].clone()) {
-                                Ok(dictionary) => {
-                                        println!("Opened dictionary with {} patterns...", dictionary.patterns.clone().expect("").len());
-                                        dictionary
-                                },
-                                Err(e) => return Err(format!("Error: {}", e)),
-                        };
-                        Ok(dictionary)
-                },
-                _ => Err(format!("Error: {}", response.status())),
-        }
+	match response.status() {
+		StatusCode::NOT_FOUND => {
+			println!("Creating new dictionary...");
+			Ok(Dictionary::new(name, Constraints::default(), false))
+		}
+		StatusCode::OK => {
+			let body: Value = response.json().await.unwrap();
+			let dictionary: Dictionary = match serde_json::from_value::<Dictionary>(body[name].clone()) {
+				Ok(dictionary) => {
+					println!("Opened dictionary with {} patterns...", dictionary.patterns.clone().expect("").len());
+					dictionary
+				}
+				Err(e) => return Err(format!("Error: {}", e)),
+			};
+			Ok(dictionary)
+		}
+		_ => Err(format!("Error: {}", response.status())),
+	}
 }
 
 async fn save_dictionary(dictionary: &Dictionary) -> Result<(), String> {
-        println!("Saving Dictionary with {} patterns...", dictionary.patterns.clone().expect("").len());
+	println!("Saving Dictionary with {} patterns...", dictionary.patterns.clone().expect("").len());
 
-        // create dictionary bucket
-        let client = reqwest::Client::new();
-        let url = format!("http://127.0.0.1:8515/bucket?name={}&type=object", "dictionary");
-        match client.post(&url).send().await {
-                Ok(_) => (),
-                Err(e) => return Err(format!("Error: {}", e)),
-        };
+	// create dictionary bucket
+	let client = reqwest::Client::new();
+	let url = format!("http://127.0.0.1:8515/bucket?name={}&type=object", "dictionary");
+	match client.post(&url).send().await {
+		Ok(_) => (),
+		Err(e) => return Err(format!("Error: {}", e)),
+	};
 
-        // add dictionary to bucket
-        let url = format!("http://127.0.0.1:8515/bucket/dictionary/{}", dictionary.name);
-        let body = json!(dictionary);
-        match client.put(&url).json(&body).send().await {
-                Ok(response) => {
-                        if response.status() != StatusCode::OK {
-                                return Err(format!("Error: {}", response.status()));
-                        }
-                },
-                Err(e) => return Err(format!("Error: {}", e)),
-        };
+	// add dictionary to bucket
+	let url = format!("http://127.0.0.1:8515/bucket/dictionary/{}", dictionary.name);
+	let body = json!(dictionary);
+	match client.put(&url).json(&body).send().await {
+		Ok(response) => {
+			if response.status() != StatusCode::OK {
+				return Err(format!("Error: {}", response.status()));
+			}
+		}
+		Err(e) => return Err(format!("Error: {}", e)),
+	};
 
-        Ok(())
- }
-
+	Ok(())
+}
 
 async fn delete_batches(patterns: Vec<Pattern>) {
-        let client = reqwest::Client::new();
-        for pattern in patterns {
-                let occurrences: Vec<Occurrence> = pattern.occurrences.values().cloned().collect();
-                for occurrence in occurrences {
-                        let url = format!("http://127.0.0.1:8515/bucket/batch/{}", occurrence.id);
-                        client.delete(&url).send().await.unwrap();
-                }
-        }
+	let client = reqwest::Client::new();
+	for pattern in patterns {
+		let occurrences: Vec<Occurrence> = pattern.occurrences.values().cloned().collect();
+		for occurrence in occurrences {
+			let url = format!("http://127.0.0.1:8515/bucket/batch/{}", occurrence.id);
+			client.delete(&url).send().await.unwrap();
+		}
+	}
 }
