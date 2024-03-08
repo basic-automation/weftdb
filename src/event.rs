1c935ff4c31712a81cce2e6654c04cd28fd501d8
Justin Icenhour <creative515.justin@gmail.com>
2024-03-07 21:54:52 -0600

    Changes

--- src/event.rs
+++ src/event.rs
@@ -5,12 +5,12 @@ use serde::{Deserialize, Serialize};
 pub struct MeasurementEvent {
 	pub event_type: String,
 	pub key: BigDecimal,
-	pub dataset_name: Option<String>,
+	pub source: Option<String>,
 	pub timestamp: BigDecimal,
 }
 
 impl MeasurementEvent {
-	pub fn new(event_type: &str, key: BigDecimal, dataset_name: Option<String>) -> Self {
-		Self { event_type: event_type.to_string(), key, dataset_name, timestamp: BigDecimal::from_i64(chrono::Utc::now().timestamp()).unwrap() }
+	pub fn new(event_type: &str, key: BigDecimal, source: Option<String>) -> Self {
+		Self { event_type: event_type.to_string(), key, source, timestamp: BigDecimal::from_i64(chrono::Utc::now().timestamp()).unwrap() }
 	}
 }
